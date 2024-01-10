4383c8b0233a8503e30c0e3ada8af0b229f0f9af
kinjo-ryura <rk020805@icloud.com>
2024-01-10 18:57:50 +0900

    Initial commit

---
+++ handballAnalysisApp/handballAnalysisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  handballAnalysisAppApp.swift
+//  handballAnalysisApp
+//
+//  Created by 金城瑠羅 on 2024/01/10.
+//
+
+import SwiftUI
+
+@main
+struct handballAnalysisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
